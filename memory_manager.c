4660da98187263e7a564b07ab85cb95eb1ba6b65
ledacvien <53806253+ledacvien@users.noreply.github.com>
2023-04-22 01:44:16 -0700

    Add files via upload

--- memory_manager.c
+++ memory_manager.c
@@ -4,13 +4,69 @@
 #include <linux/mm.h>
 #include <linux/sched/mm.h>
 #include <linux/module.h>
+#include <linux/pid.h> //i added this for get_pid_task pid maybe not right?
+
+
 
 
 unsigned long timer_interval_ns = 10e9; // 10-second timer
 static struct hrtimer hr_timer;
-int RSS, WSS, SWAP, pid;
+int pid;
+int WSS = 0;
+int RSS = 0;
+int SWAP = 0;
 module_param(pid, int, 0);
-//int ptep_test_and_clear_young (struct vm_area_struct *vma, unsigned long addr, pte_t *ptep);
+unsigned long page;
+
+
+void walk_pte_in_memory(const struct mm_struct *const mm, const unsigned long address)
+{
+	pgd_t *pgd;
+	p4d_t *p4d;
+	pud_t *pud;
+	pmd_t *pmd;
+	pte_t *ppte;
+	
+	pgd = pgd_offset(mm, address);
+	if (pgd_none(*pgd) || pgd_bad(*pgd))
+		return;
+	
+	p4d = p4d_offset(pgd, address);
+	if (p4d_none(*p4d) || p4d_bad(*p4d))
+		return;
+	
+	pud = pud_offset(p4d, address);
+	if (pud_none(*pud) || pud_bad(*pud))
+		return;
+	
+	pmd = pmd_offset(pud, address);
+	if (pmd_none(*pmd) || pmd_bad(*pmd))
+		return;
+	
+	ppte = pte_offset_map(pmd, address);
+	if (!ppte)
+		return;
+
+    pte_t pte;
+
+    pte = *ppte;
+
+        if(!pte_none(pte)){
+		if(pte_present(pte)) {
+			RSS++;
+			if(pte_young(pte)){
+				WSS++;
+				test_and_clear_bit(address, (unsigned long *)ppte);
+			}
+		} 	
+		else {
+			SWAP++;
+		}
+	}    
+	
+	
+}
+
 
 enum hrtimer_restart timer_callback( struct hrtimer *timer_for_restart )
 {
@@ -22,68 +78,30 @@ enum hrtimer_restart timer_callback( struct hrtimer *timer_for_restart )
 
 	// Do the measurement, like looking into VMA and walking through memory pages
 	// pte_t *ppte, pte; ppte means 'pointer of PTE'
-	task_struct *task;
-	pte_t *ptep = search_pte_in_memory(task->mm, task->address); // missing mm_struct and address, don't know how to find them
-	pte = *ppte;
-	If pte exists {
-		If pte is present {
-			RSS++
-			If pte is young {
-				WSS++
-				test_and_clear_bit(_PAGE_BIT_ACCESSED,(unsigned long *)ppte);
-			}
-		} 	
-		Else {
-			SWAP++
-		}
-	}
+
+
+
+	struct task_struct *task = get_pid_task(pid, PIDTYPE_PID);
+
+    	struct vm_area_struct *virtual_memory = task->mm->mmap;
+
+    	while(virtual_memory){
+
+    		for(page = (unsigned long) virtual_memory->vm_start; page < (unsigned long) virtual_memory->vm_end; page+=PAGE_SIZE)
+    		{
+        		walk_pte_in_memory(task->mm, page);
+    		}
+        	virtual_memory = virtual_memory->vm_next;
+    	}
+
+	
 
 	// And also do the Kernel log printing aka printk per requirements
-	printk(KERN_INFO "PID [%s]: RSS=[%d] KB, SWAP=[%d] KB, WSS=[%d] KB", PID,RSS,SWAP,WSS);
+	printk(KERN_INFO "PID [%s]: RSS=[%d] KB, SWAP=[%d] KB, WSS=[%d] KB", pid,RSS,SWAP,WSS);
 
 	return HRTIMER_RESTART;
 }
 
-/*
-int ptep_test_and_clear_young (struct vm_area_struct *vma, unsigned long addr, pte_t *ptep)
-{
-	int ret = 0;
-	if (pte_young(*ptep))
-		ret = test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte);
-	return ret;
-}
-*/
-
-struct pte_t *search_pte_in_memory(const struct mm_struct *const mm, const unsigned long address)
-{
-	pgd_t *pdg;
-	p4d_t *p4d;
-	pud_t *pud;
-	pmd_t *pmd;
-	pte_t *ptep;
-	
-	pgd = pgd_offset(mm, address);
-	if (pgd_none(*pgd) || pgd_bad(*pgd))
-		return NULL;
-	
-	p4d = p4d_offset(pgd, address);
-	if (p4d_none(*p4d) || p4d_bad(*p4d))
-		return NULL;
-	
-	pud = pud_offset(p4d, address);
-	if (pud_none(*pud) || pud_bad(*pud))
-		return NULL;
-	
-	pmd = pmd_offset(pud, address);
-	if (pmd_none(*pmd) || pmd_bad(*pmd))
-		return NULL;
-	
-	ptep = pte_offset_map(pmd, address);
-	if (!ptep)
-		return NULL;
-	
-	return ptep;
-}
 
 static int __init timer_init(void) {
 	ktime_t ktime ;
@@ -104,3 +122,4 @@ static void __exit timer_exit(void) {
 
 module_init(timer_init);
 module_exit(timer_exit);
+MODULE_LICENSE("GPL");
